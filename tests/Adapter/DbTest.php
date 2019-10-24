c454e4851934de26f8c2eb1e2c22297ab54e91a9
Nick Sagona <nick@nolainteractive.com>
2019-10-24 14:11:58 -0500

    Work on unit test

--- tests/Adapter/DbTest.php
+++ tests/Adapter/DbTest.php
@@ -18,7 +18,48 @@ class DbTest extends TestCase
         ]);
         $adapter = new Adapter\Db($db);
         $this->assertInstanceOf('Pop\Queue\Adapter\Db', $adapter);
+        $this->assertInstanceOf('Pop\Db\Adapter\Sqlite', $adapter->db());
+        $this->assertEquals('pop_queue_jobs', $adapter->getTable());
+        $this->assertEquals('pop_queue_failed_jobs', $adapter->getFailedTable());
+    }
+
+    public function testGetJobs()
+    {
+        $db = Db\Db::sqliteConnect([
+            'database' => __DIR__ . '/../tmp/test.sqlite'
+        ]);
+        $adapter = new Adapter\Db($db);
+        $this->assertFalse($adapter->hasJob(1));
+        $this->assertNull($adapter->getJob(1));
+        $this->assertFalse($adapter->hasJobs('pop-queue'));
+        $this->assertEmpty($adapter->getJobs('pop-queue'));
+    }
+
+    public function testGetCompletedJobs()
+    {
+        $db = Db\Db::sqliteConnect([
+            'database' => __DIR__ . '/../tmp/test.sqlite'
+        ]);
+        $adapter = new Adapter\Db($db);
+        $this->assertFalse($adapter->hasCompletedJob(1));
+        $this->assertNull($adapter->getCompletedJob(1));
+        $this->assertFalse($adapter->hasCompletedJobs('pop-queue'));
+        $this->assertEmpty($adapter->getCompletedJobs('pop-queue'));
+    }
+
+    public function testGetFailedJobs()
+    {
+        $db = Db\Db::sqliteConnect([
+            'database' => __DIR__ . '/../tmp/test.sqlite'
+        ]);
+        $adapter = new Adapter\Db($db);
+        $this->assertFalse($adapter->hasFailedJob(1));
+        $this->assertNull($adapter->getFailedJob(1));
+        $this->assertFalse($adapter->hasFailedJobs('pop-queue'));
+        $this->assertEmpty($adapter->getFailedJobs('pop-queue'));
+
         unlink(__DIR__ . '/../tmp/test.sqlite');
     }
+
 
 }
