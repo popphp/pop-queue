198efe0636a73c9e91fedf9abcc8dc7226f9206b
Nick Sagona <nick@noladev.com>
2026-08-11 19:48:22 -0500

    Update copyright

--- src/Queue.php
+++ src/Queue.php
@@ -4,7 +4,7 @@
  *
  * @link       https://github.com/popphp/popphp-framework
  * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
+ * @copyright  Copyright (c) 2009-2027 NOLA Interactive, LLC.
  * @license    https://www.popphp.org/license     New BSD License
  */
 
@@ -28,7 +28,7 @@ use Pop\Queue\Process\TimeoutException;
  * @category   Pop
  * @package    Pop\Queue
  * @author     Nick Sagona, III <dev@noladev.com>
- * @copyright  Copyright (c) 2009-2026 NOLA Interactive, LLC.
+ * @copyright  Copyright (c) 2009-2027 NOLA Interactive, LLC.
  * @license    https://www.popphp.org/license     New BSD License
  * @version    3.0.0
  */
